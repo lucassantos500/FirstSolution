ce7a133b3aa5d1b2d24269e265ce6c43bb5bd196
lucassantos500 <lucassantos500@hotmail.com>
2018-07-27 14:43:14 -0300

    Aula 1 - HelloWorld e História Aula 2 - Tipos de variáveis

---
+++ FirstSolution/Type/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Type
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = 10;//Criando uma variável do tipo int
+            string B = "Olá";//Criando uma variável do tipo string
+            float VERDE;//Variável com sublinhado verde = Não instânciada
+            bool valor = true;//ou false
+
+
+            Console.WriteLine(A);//Imprimindo a variavel A na tela
+            Console.WriteLine(B);//Imprimindo a variavel B na tela
+            Console.ReadLine();
+        }
+    }
+}
