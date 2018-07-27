8a337964c550f5fa813a4d4e3abe87374e5488e1
lucassantos500 <lucassantos500@hotmail.com>
2018-07-27 14:49:54 -0300

    Aula - HelloWorld, Tipo variável, Enum

--- FirstSolution/Type/Program.cs
+++ FirstSolution/Type/Program.cs
@@ -12,9 +12,13 @@ namespace Type
         {
             int A = 10;//Criando uma variável do tipo int
             string B = "Olá";//Criando uma variável do tipo string
-            float VERDE;//Variável com sublinhado verde = Não instânciada
+            char ch = 'A';//Criando uma variável do tipo char
             bool valor = true;//ou false
+            float ff = 10.89F;//Criando uma variável do tipo float
+            double dd = 90.9876;//Criando uma variável do tipo decimal
+            decimal dec = 10.88M;//Criando uma variável do tipo dec
 
+            float VERDE;//Variável com sublinhado verde = Não instânciada
 
             Console.WriteLine(A);//Imprimindo a variavel A na tela
             Console.WriteLine(B);//Imprimindo a variavel B na tela
